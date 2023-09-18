b18b38e93eb659f9d4d65986b1c1349083404a9d
Yannis <72886828+ioannispanges@users.noreply.github.com>
2023-09-18 11:27:10 +0300

    Delete Παραδοτέο Π0.2.1/Crypto-Library/core directory

--- Παραδοτέο Π0.2.1/Crypto-Library/core/src/main/java/eu/olympus/util/multisign/MSverfKey.java
+++
@@ -1,9 +0,0 @@
-package eu.olympus.util.multisign;
-
-import java.security.PublicKey;
-
-/**
- * Interface for the verification keys that the scheme uses.
- */
-public interface MSverfKey extends PublicKey {
-}
--- Παραδοτέο Π0.2.1/Crypto-Library/core/src/main/java/eu/olympus/util/rangeProof/RangePredicateVerificationResult.java
+++
@@ -1,7 +0,0 @@
-package eu.olympus.util.rangeProof;
-
-// We could be more precise (which proof failed)
-public enum RangePredicateVerificationResult {
-    VALID,
-    INVALID
-}
